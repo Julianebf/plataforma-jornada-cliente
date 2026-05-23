767a38fb375063cfc76ac50cf9d98c8407850b09
julianebf <fbjuliane6@gmail.com>
2026-05-23 15:36:14 -0300

    ajustando aquivo exportavel

--- frontend/app/page.tsx
+++ frontend/app/page.tsx
@@ -79,7 +79,7 @@ export default function DashboardPage() {
 const pdf = new jsPDF({
   orientation: "landscape",
   unit: "px",
-  format: [1400, 1800],
+  format: [1400, 2000],
 });
 
 const imgProps =
